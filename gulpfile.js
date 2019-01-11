80c2a71d72ebb80bfd2ba470302491840152c246
steekam <waynewanyee@gmail.com>
2019-01-12 01:26:56 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+// Compile Sass
+gulp.task('sass', done => {
+    gulp.src('scss/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+    done();
+});
+
+// Watch & serve
+gulp.task('serve', done => {
+    browserSync.init({
+        server: './dist'
+    });
+
+    gulp.watch('scss/*.scss',gulp.series('sass'));
+    gulp.watch('dist/*.html').on('change',browserSync.reload);
+    done();
+});
+
+// Default
+gulp.task('default', gulp.series('sass','serve'));
